b5df16c1dcc6708d313b45e12c3d0820e2eae444
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2016-03-28 17:03:27 -0400

    Add comments for the lines written so far

--- annotator.m
+++ annotator.m
@@ -1,14 +1,27 @@
+%%% Simple video annotation tool
+%%% Murat Ambarkutuk
+%%% 03/28/2016
+%%% Virginia Tech
+%% Clear everything
 clc; clear all; close all;
-
+%% Create a video reader object
+% If this line crashes the script, 
+% it is likely to result from a missing G-Streamer plugin or G-Streamer
+% itself; most likely missing plugin: gstreamer0.10-ffmpeg plugin 
 vidObj = VideoReader('video.mp4');
-figure;
+%% Loop over for each frame
 while hasFrame(vidObj)
+    % Obtain the frame
     vidFrame = readFrame(vidObj);
-%     image(vidFrame, 'Parent', currAxes);
-%     currAxes.Visible = 'off';
+    % Create a new empty frame with the same size of the input frame
+    target = zeros(size(vidFrame,1), size(vidFrame,2));
+    % Show the frame
     figure(1); imshow(vidFrame);
+    % Get the rectangle drawn on the figure
     posRect = getrect;
+    % Display the rectangle
     rectangle('Position', posRect);
     figure(2);
+    % Show the new image
     pause;
 end
