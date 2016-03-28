109f2d5e0a8ece5495f602c68233a53dd5e76f01
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2016-03-28 16:39:31 -0400

    Initial Commit

---
+++ annotater.m
@@ -0,0 +1,13 @@
+clc; clear all; close all;
+
+vidObj = VideoReader('video.mp4');
+figure;
+while hasFrame(vidObj)
+    vidFrame = readFrame(vidObj);
+%     image(vidFrame, 'Parent', currAxes);
+%     currAxes.Visible = 'off';
+    figure(1); imshow(vidFrame);
+    posRect = getrect
+%     rectangle('Position',posRect);
+    pause;
+end
