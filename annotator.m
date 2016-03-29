2f57e9f44699d29275411d8e6b5c2f75d00e33d1
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2016-03-29 10:26:43 -0400

    Add dialog asking user to save the frame

--- annotator.m
+++ annotator.m
@@ -8,20 +8,43 @@ clc; clear all; close all;
 % If this line crashes the script, 
 % it is likely to result from a missing G-Streamer plugin or G-Streamer
 % itself; most likely missing plugin: gstreamer0.10-ffmpeg plugin 
-vidObj = VideoReader('video.mp4');
+fileName = 'video.mp4';
+fileToSave = [fileName,'.mat'];
+vidObj = VideoReader(fileName);
 %% Loop over for each frame
+frameNum = 1;
 while hasFrame(vidObj)
     % Obtain the frame
     vidFrame = readFrame(vidObj);
     % Create a new empty frame with the same size of the input frame
-    target = zeros(size(vidFrame,1), size(vidFrame,2));
+    target = logical(false(size(vidFrame,1), size(vidFrame,2)));
     % Show the frame
-    figure(1); imshow(vidFrame);
+    figure(1); imshow(vidFrame); title('Draw a rectangle w/ mouse');
     % Get the rectangle drawn on the figure
-    posRect = getrect;
-    % Display the rectangle
-    rectangle('Position', posRect);
-    figure(2);
-    % Show the new image
-    pause;
+    confirmed = false;
+    while ~confirmed
+        posRect = getrect;
+        % Display the rectangle
+        rectangle('Position', posRect);
+        % Area drawn
+        x_min = int16(posRect(1));
+        x_max = int16(posRect(1) + posRect(3));
+        y_min = int16(posRect(2));
+        y_max = int16(posRect(2) + posRect(4));
+        target(y_min:y_max, x_min:x_max) = true; 
+        annotation(:,:,frameNum) = target;
+        % Show the new image
+%         figure(2); imshow(target);
+        figure(3); imshow(vidFrame(y_min:y_max, x_min:x_max)); title('Selected Region');
+        button = questdlg('Do you want to save it the frame?','Confirmation')
+        if isempty(button)
+            str = 'Yes';
+            save(fileToSave, 'annotation');
+            frameNum = frameNum + 1;
+            close(3);
+            confirmed = true;
+        else          
+            close(3);
+        end
+    end
 end
