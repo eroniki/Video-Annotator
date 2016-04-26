7a55f36897a60452b365004e0fb789c3a6539201
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2016-04-26 16:11:02 -0400

    Add HoG feature representation for the ROI and change output format

--- annotator.m
+++ annotator.m
@@ -4,12 +4,6 @@
 %%% Virginia Tech
 %% Clear everything
 clc; clear all; close all;
-
-%% Make VLFeat Ready
-run('~/vlfeat/toolbox/vl_setup');
-disp('done');
-
-
 %% Create a video reader object
 % If this line crashes the script, 
 % it is likely to result from a missing G-Streamer plugin or G-Streamer
@@ -20,10 +14,12 @@ vidObj = VideoReader(fileName);
 %% Loop over for each frame
 frameNum = 1;
 while hasFrame(vidObj)
+%     frameNum
     % Obtain the frame
     vidFrame = readFrame(vidObj);
+    % Extract HoG features for the frame
     % Create a new empty frame with the same size of the input frame
-    target = logical(false(size(vidFrame,1), size(vidFrame,2)));
+    mask = logical(false(size(vidFrame,1), size(vidFrame,2)));
     % Show the frame
     figure(1); imshow(vidFrame); title('Draw a rectangle w/ mouse');
     % Get the rectangle drawn on the figure
@@ -37,19 +33,23 @@ while hasFrame(vidObj)
         x_max = int16(posRect(1) + posRect(3));
         y_min = int16(posRect(2));
         y_max = int16(posRect(2) + posRect(4));
-        target(y_min:y_max, x_min:x_max) = true; 
-        annotation(:,:,frameNum) = target;
+        mask(y_min:y_max, x_min:x_max) = true; 
+        annotation(frameNum).mask = mask;
+        annotation(frameNum).target = imresize(vidFrame(y_min:y_max, x_min:x_max), [50, 50]);
+        [annotation(frameNum).features, annotation(frameNum).hogVisualization] = extractHOGFeatures(annotation(frameNum).target);
+        size(annotation(frameNum).features) 
         % Show the new image
-%         figure(2); imshow(target);
+        figure(2); imshow(mask);
         figure(3); imshow(vidFrame(y_min:y_max, x_min:x_max)); title('Selected Region');
+        figure(4); plot(annotation(frameNum).hogVisualization);
         button = questdlg('Do you want to save it the frame?','Confirmation')
         if strcmp(button,'Yes')
             save(fileToSave, 'annotation');
             frameNum = frameNum + 1;
-            close(3);
             confirmed = true;
-        else          
-            close(3);
         end
+        close(2);
+        close(3);
+        close(4);
     end
 end
